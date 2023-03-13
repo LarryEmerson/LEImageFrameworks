fa37aab9e2eeea529e90b1fd7f0bd9e0bbe820d1
LarryEmerson <larryemerson@163.com>
2023-03-13 10:59:56 +0800

    问题修复

--- LEImageFrameworks/Classes/LESingleImagePicker.m
+++ LEImageFrameworks/Classes/LESingleImagePicker.m
@@ -39,7 +39,7 @@
     [navi leSetRightNavigationItemWith:@"完成" Image:nil];
     [navi leSetLeftNavigationItemWith:@"取消" Image:nil Color:nil];
     [self.leViewBelowCustomizedNavigation setBackgroundColor:[UIColor colorWithRed:0.412 green:0.396 blue:0.409 alpha:1.000]];
-    scrollView=[[UIScrollView alloc] initWithAutoLayoutSettings:[[LEAutoLayoutSettings alloc] initWithSuperView:self.leViewBelowCustomizedNavigation Anchor:LEAnchorInsideCenter Offset:CGPointZero CGSize:CGSizeMake(LESCREEN_WIDTH, self.leViewBelowCustomizedNavigation.bounds.size.height/curAspect)]];
+    scrollView=[[UIScrollView alloc] initWithAutoLayoutSettings:[[LEAutoLayoutSettings alloc] initWithSuperView:self.leViewBelowCustomizedNavigation Anchor:LEAnchorInsideCenter Offset:CGPointZero CGSize:CGSizeMake(LESCREEN_WIDTH, LESCREEN_WIDTH/curAspect)]];
     [scrollView setBackgroundColor:LEColorClear];
     [scrollView setDelegate:self];
     [scrollView setShowsHorizontalScrollIndicator:NO];
@@ -47,7 +47,7 @@
     [scrollView setMaximumZoomScale:100];
     //    [self.leViewBelowCustomizedNavigation addSubview:scrollView];
     float minWA=LESCREEN_WIDTH*1.0/curImage.size.width;
-    float minHA=self.leViewBelowCustomizedNavigation.bounds.size.height*1.0/curAspect/curImage.size.height;
+    float minHA=LESCREEN_WIDTH*1.0/curAspect/curImage.size.height;
     float miniAspect=minWA>minHA?minWA:minHA;
     imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, curImage.size.width, curImage.size.height)];
     [imageView setImage:curImage];
@@ -59,7 +59,7 @@
     [scrollView setMaximumZoomScale:2.5<miniAspect?miniAspect:2.5];
     [scrollView setClipsToBounds:NO];
     if(curAspect==1){
-        UIView *viewCover=[UIView new].leSuperView(self.leViewBelowCustomizedNavigation).leAnchor(LEAnchorInsideCenter).leSize(scrollView.bounds.size).leAutoLayout;
+        UIView *viewCover=[UIView new].leSuperView(self.leViewBelowCustomizedNavigation).leAnchor(LEAnchorInsideCenter).leSize(self.leViewBelowCustomizedNavigation.bounds.size).leAutoLayout;
         [viewCover setUserInteractionEnabled:NO];
         [[LEUIFramework getTransparentCircleLayerForView:viewCover Diameter:LESCREEN_WIDTH MaskColor:LEColorMask5] setUserInteractionEnabled:NO];
     }else{
